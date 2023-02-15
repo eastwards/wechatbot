97e9933ff8e23617488bebae1bc39cf49681144d
Chris <xtvj007@qq.com>
2023-02-15 16:27:00 +0800

    test

--- handlers/handler.go
+++ handlers/handler.go
@@ -34,35 +34,39 @@ func Handler(msg *openwechat.Message) {
 	log.Printf("hadler Received msg : %v", msg.Content)
 	log.Printf("msg Status:%v, StatusNotifyCode:%v, CreateTime:%v, SystemNow:%v", msg.Status, msg.StatusNotifyCode, msg.CreateTime, time.Now().Unix());
 
-	// 如果已接受过，不再进行处理
-	if msg.IsNotify() {
-		log.Printf("msg was Notified. never explain.");
-		return
-	}
-	
-	// 如果消息时间超过10秒，不再处理
-	if 10 <  time.Now().Unix() - msg.CreateTime {
-		log.Printf("msg was expired. never explain.");
-		return
-	}
+	// 多线程处理
+	go func() {
 
-	// 处理群消息
-	if msg.IsSendByGroup() {
-		handlers[GroupHandler].handle(msg)
-		return
-	}
+		// 如果已接受过，不再进行处理
+		if msg.IsNotify() {
+			log.Printf("msg was Notified. never explain.");
+			return
+		}
+		
+		// 如果消息时间超过10秒，不再处理
+		if 10 <  time.Now().Unix() - msg.CreateTime {
+			log.Printf("msg was expired. never explain.");
+			return
+		}
 
-	// 好友申请
-	if msg.IsFriendAdd() {
-		if config.LoadConfig().AutoPass {
-			_, err := msg.Agree("你好我是基于chatGPT引擎开发的微信机器人，你可以向我提问任何问题。")
-			if err != nil {
-				log.Fatalf("add friend agree error : %v", err)
-				return
+		// 处理群消息
+		if msg.IsSendByGroup() {
+			handlers[GroupHandler].handle(msg)
+			return
+		}
+
+		// 好友申请
+		if msg.IsFriendAdd() {
+			if config.LoadConfig().AutoPass {
+				_, err := msg.Agree("你好我是基于chatGPT引擎开发的微信机器人，你可以向我提问任何问题。")
+				if err != nil {
+					log.Fatalf("add friend agree error : %v", err)
+					return
+				}
 			}
 		}
-	}
 
-	// 私聊
-	handlers[UserHandler].handle(msg)
+		// 私聊
+		handlers[UserHandler].handle(msg)
+	}
 }
